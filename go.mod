63a02755d12cc3acce38949f95bb415038afed19
Mendel Greenberg <chabad360@gmail.com>
2021-03-02 22:30:28 -0500

    remove gio

--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module github.com/chabad360/resolume-timecode
 
 go 1.15
 
-require (
-	gioui.org v0.0.0-20210209100840-ebfb17ec6c56
-	github.com/hypebeast/go-osc v0.0.0-20200115085105-85fee7fed692 // indirect
-	golang.org/x/exp v0.0.0-20210201131500-d352d2db2ceb // indirect
-)
+require github.com/hypebeast/go-osc v0.0.0-20200115085105-85fee7fed692
--- main.go
+++ main.go
@@ -2,25 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"os"
 	"strconv"
 	"strings"
 	"time"
 
-	"gioui.org/app"
-	"gioui.org/font/gofont"
-	"gioui.org/io/system"
-	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/unit"
-	//"gioui.org/widget"
-	"gioui.org/widget/material"
 	"github.com/hypebeast/go-osc/osc"
-
-
-	//"golang.org/x/exp/shiny/materialdesign/icons"
 )
 
 func main() {
@@ -42,7 +30,7 @@ func main() {
 
 	go func() {
 		fmt.Println("Start listening on", addr)
-
+		//fmt.Println("pos,average,time,timeAverage,timeActual,timeTotal,timeLeft,timeNow")
 		for {
 			packet, err := server.ReceivePacket(conn)
 			timeNow = time.Now()
@@ -59,73 +47,30 @@ func main() {
 		message := osc.NewMessage("/composition/selectedclip/name")
 		message.Append("?")
 		for {
-			<- ticker2.C
+			<-ticker2.C
 			client.Send(message)
 		}
 	}()
 
-	go func() {
-		w = app.NewWindow(app.Size(unit.Dp(750), unit.Dp(120)), app.Title("Resolume Time"))
-		if err := loop(w); err != nil {
-			log.Fatal(err)
-		}
-		os.Exit(0)
-	}()
-	app.Main()
-}
-
-func loop(w *app.Window) error {
-	th := material.NewTheme(gofont.Collection())
-
-	var ops op.Ops
 	for {
-		select {
-		case e := <-w.Events():
-			switch e := e.(type) {
-			case system.DestroyEvent:
-				return e.Err
-			case system.FrameEvent:
-				// Reset the layout.Context for a new frame.
-				gtx := layout.NewContext(&ops, e)
-
-				// Draw the state into ops based on events in e.Queue.
-				draw(gtx, th)
-
-				// Update the display.
-				e.Frame(gtx.Ops)
-			}
-		}
 	}
 }
 
-
-func draw(gtx layout.Context, th *material.Theme) layout.Dimensions {
-	return layout.Flex{
-		Axis: layout.Vertical,
-		Alignment: layout.Middle,
-	}.Layout(gtx,
-		layout.Rigid(material.H1(th, timeLeft).Layout),
-	)
-}
-
 var (
-	posPrev  float32
-	timePrev time.Time
-	timeNow time.Time
-	tPrev int
-	samples int
-	timeLeft string
-	interval float32
-	duration float32 = 10000
-	div float32 = 1000
-	array = []float32{0}
-	timeArray = []float32{0}
-	totalArray = []float32{0}
-	name string
-	w *app.Window
+	posPrev    float32
+	timePrev   time.Time
+	timeNow    time.Time
+	tPrev      int
+	samples    int
+	timeLeft   string
+	interval   float32
+	duration   float32 = 10000
+	div        float32 = 1000
+	array              = []float32{0}
+	timeArray          = []float32{0}
+	totalArray         = []float32{0}
+	name       string
 )
-
-//const duration float32 = 10
 
 func Pop(array []float32) (values []float32, value float32) {
 	value, values = array[0], array[1:]
@@ -142,15 +87,15 @@ func maxAppend(array []float32, value float32, limit int) []float32 {
 
 func average(array []float32) float32 {
 	var f float32
-	for i := 0; i<len(array); i++ {
+	for i := 0; i < len(array); i++ {
 		f = f + array[i]
 	}
-	return f/float32(len(array))
+	return f / float32(len(array))
 }
 
 func within(original float32, new float32, percent float32) bool {
 	p := original / 100 * percent
-	if (new > original + p || new < original - p) && original != 0 {
+	if (new > original+p || new < original-p) && original != 0 {
 		return false
 	}
 	return true
@@ -160,49 +105,61 @@ func clipName(msg *osc.Message) {
 	nameString := strings.TrimPrefix(msg.String(), "/composition/selectedclip/name ,s ")
 	if name != nameString {
 		name = nameString
-		samples = 0
+		reset()
 	}
+}
+
+func reset() {
+	samples = 0
+	totalArray = []float32{0}
+	timeArray = []float32{0}
+	array = []float32{0}
 }
 
 func procMsg(msg *osc.Message) {
 	var t int
 	var timeActual time.Duration
+	var tA float32
+	var td float32
 	posString := strings.TrimPrefix(msg.String(), "/composition/selectedclip/transport/position ,f ")
 	pos64, _ := strconv.ParseFloat(posString, 32)
 	pos := float32(pos64)
+	if pos < 0.000005 {
+		reset()
+	}
 
 	a := average(array)
 	ta := average(timeArray)
-	i := pos-posPrev
+	i := pos - posPrev
 	d := float32(timeNow.Sub(timePrev).Microseconds())
-	if (i == 0 || d == 0) {
+	if i == 0 || d == 0 {
 		goto done
 	}
-	if !within(ta, d, 50) && samples > 500{
-		fmt.Printf("w")
+	if !within(ta, d, 50) && samples > 500 {
+		//fmt.Printf("w")
 	} else {
 		timeArray = maxAppend(timeArray, d, 100)
 		array = maxAppend(array, i, 100)
 		interval = average(array)
-		duration = average(timeArray)/div
+		duration = average(timeArray) / div
 
-		td := duration*(1/interval)
-		tA := average(totalArray)
-		if within(tA, td, 0.001) && samples > 1000 && samples < 3000{
+		td = duration * (1 / interval)
+		tA = average(totalArray)
+		if within(tA, td, 0.001) && samples > 1000 && samples < 2000 {
 			totalArray = maxAppend(totalArray, td, 500)
 		} else if within(tA, td, 1) && samples > 500 {
-			totalArray = maxAppend(totalArray, td, 500)
-		} else if samples < 500{
+			totalArray = maxAppend(totalArray, td, 250)
+		} else if samples < 500 {
 			totalArray = maxAppend(totalArray, td, 100)
 		}
 	}
 	samples++
 
-
 	//t = (duration/div)*((1-pos)/interval)
 	//t = (duration/div)*((1-pos)/(interval/samples))
 	//t = int((duration)*((1-pos)/interval))
-	t = int((average(totalArray)/1)*(1-pos))
+	t = int((average(totalArray) / 1) * (1 - pos))
+	//t = int(100000/1)*(1-pos))
 
 	posPrev = pos
 	timePrev = timeNow
@@ -211,9 +168,7 @@ func procMsg(msg *osc.Message) {
 		int(timeActual.Minutes())-(60*int(timeActual.Hours())),
 		int(timeActual.Seconds())-(60*int(timeActual.Minutes())),
 		int(timeActual.Milliseconds()-(1000*int64(timeActual.Seconds()))))
-	fmt.Printf("pos: %f\taverage: %f\t timeAverage: %f\ti: %f\ttimePrev: %d\ttimeActual: %d\ttimeTotal: %d\ttimeLeft: %s\tname: %s\n", pos, a, ta, interval, tPrev, t, int(average(totalArray)), timeLeft, name)
+	fmt.Printf("pos: %f\taverage: %f\ti: %f\ttime: %f\ttimeAverage: %f\ttimeActual: %d\ttimeTotal: %d\ttimeLeft: %s\n", pos, a, interval, d, ta, t, int(average(totalArray)), timeLeft)
+	//fmt.Printf("%f,%f,%f,%f,%f,%d,%d,%d\n", pos, a, interval, d, ta, t, int(average(totalArray)),timeNow.UnixNano())
 done:
-	if w != nil {
-		w.Invalidate()
-	}
 }
