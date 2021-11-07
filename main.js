04d009368988bf8356048c5fcdeae3e58017ace3
Mendel Greenberg <chabad360@gmail.com>
2021-11-07 14:15:59 -0500

    chore: refactor

--- main.js
+++ main.js
@@ -2,21 +2,22 @@
 
 const socket            = new WebSocket(`ws://${location.host}/ws`);
 
-const timecodehours     = document.getElementById("timecode-hours");
-const timecodeminutes   = document.getElementById("timecode-minutes");
-const timecodeseconds   = document.getElementById("timecode-seconds");
-const timecodems        = document.getElementById("timecode-ms");
-const timecodeclipname  = document.getElementById("clipname");
+const timecodeHours     = document.getElementById("timecode-hours");
+const timecodeMinutes   = document.getElementById("timecode-minutes");
+const timecodeSeconds   = document.getElementById("timecode-seconds");
+const timecodeMS        = document.getElementById("timecode-ms");
+const timecodeClipName  = document.getElementById("clipname");
 const table             = document.getElementById('table');
-const tableborder       = document.getElementById('tableborder');
-const cliplength        = document.getElementById("ms");
+const tableBorder       = document.getElementById('tableborder');
+const clipLength        = document.getElementById("ms");
 const status            = document.getElementById("status");
-const message          = document.getElementById("msg");
+const message           = document.getElementById("msg");
 
-const mult      = 10000000000; // This constant is used to avoid JSs famous floating point pitfalls
+const multiplier = 10000000000; // This constant is used to avoid JSs famous floating point pitfalls
 
-let clipName    = "";
+let clipName         = "";
 let directionForward = true;
+
 let timePrev    = Date();
 let posPrev     = 0;
 let samples     = 0;
@@ -38,7 +39,7 @@ function average(array){
     return Math.trunc(array.reduce((a,b) => (a+b)) / array.length);
 }
 
-function within(original, newNum, percent) {
+function isWithin(original, newNum, percent) {
     let p = (original / 100) * percent;
     return !((newNum > original + p || newNum < original - p) && original !== 0);
 }
@@ -67,14 +68,14 @@ socket.addEventListener('message', function (event) {
 socket.addEventListener('close', function () {
     status.innerHTML = "Server Stopped";
 
-    timecodehours.innerHTML = "00";
-    timecodeminutes.innerHTML = "00";
-    timecodeseconds.innerHTML = "00";
-    timecodems.innerHTML = "000";
-    cliplength.innerHTML = '0.000s'
+    timecodeHours.innerHTML = "00";
+    timecodeMinutes.innerHTML = "00";
+    timecodeSeconds.innerHTML = "00";
+    timecodeMS.innerHTML = "000";
+    clipLength.innerHTML = '0.000s'
 
     table.style.color = "#ff4545";
-    tableborder.style.borderColor = "#ff4545";
+    tableBorder.style.borderColor = "#ff4545";
 })
 
 function procDirection(data) {
@@ -84,7 +85,7 @@ function procDirection(data) {
 
 function procName(data) {
     data = data.replace("/name ,s ", "");
-    timecodeclipname.innerHTML = data;
+    timecodeClipName.innerHTML = data;
     if (data !== clipName) {
         clipName = data;
         reset();
@@ -98,11 +99,11 @@ function reset() {
     timeIntervalBuffer = [0];
     estSizeBuffer      = [0];
 
-    timecodehours.innerHTML     = '00';
-    timecodeminutes.innerHTML   = '00';
-    timecodeseconds.innerHTML   = '00';
-    timecodems.innerHTML        = '000';
-    cliplength.innerHTML        = '0.000s';
+    timecodeHours.innerHTML     = '00';
+    timecodeMinutes.innerHTML   = '00';
+    timecodeSeconds.innerHTML   = '00';
+    timecodeMS.innerHTML        = '000';
+    clipLength.innerHTML        = '0.000s';
 }
 
 async function procMsg(data) {
@@ -120,10 +121,10 @@ async function procMsg(data) {
 }
 
 function procPos(msg, timeNow) {
-    let pos = mult * parseFloat(msg.split(" ").pop());
+    let pos = multiplier * parseFloat(msg.split(" ").pop());
 
     if (!directionForward) {
-        pos = mult - pos
+        pos = multiplier - pos
     }
 
     if (pos < 50) {
@@ -131,7 +132,7 @@ function procPos(msg, timeNow) {
     }
 
     let currentPosInterval  = pos - posPrev;
-    let currentTimeInterval = (timeNow - timePrev) * mult;
+    let currentTimeInterval = (timeNow - timePrev) * multiplier;
 
     if (currentPosInterval === 0 || currentTimeInterval === 0) {
         return;
@@ -149,9 +150,9 @@ function procPos(msg, timeNow) {
 
     let currentEstSize = Math.trunc(timeInterval * (1 / posInterval));
     let prevEstSize = average(estSizeBuffer);
-    if (samples > 1000 && samples < 1500 && within(prevEstSize, currentEstSize, 0.001)) {
+    if (samples > 1000 && samples < 1500 && isWithin(prevEstSize, currentEstSize, 0.001)) {
         estSizeBuffer = maxAppend(estSizeBuffer, currentEstSize, 500);
-    } else if (samples > 500 && samples < 1000 && within(prevEstSize, currentEstSize, 1)) {
+    } else if (samples > 500 && samples < 1000 && isWithin(prevEstSize, currentEstSize, 1)) {
         estSizeBuffer = maxAppend(estSizeBuffer, currentEstSize, 250);
     } else if (samples < 500) {
         estSizeBuffer = maxAppend(estSizeBuffer, currentEstSize, 100);
@@ -159,24 +160,24 @@ function procPos(msg, timeNow) {
 
     samples++;
 
-    let t = ((average(estSizeBuffer)) * (mult - pos)) / mult;
+    let t = ((average(estSizeBuffer)) * (multiplier - pos)) / multiplier;
 
     posPrev  = pos;
     timePrev = timeNow;
 
     let timeActual = new Date(t);
-    timecodehours.innerHTML     = timeActual.getUTCHours().toString().padStart(2, '0');
-    timecodeminutes.innerHTML   = timeActual.getUTCMinutes().toString().padStart(2, '0');
-    timecodeseconds.innerHTML   = timeActual.getUTCSeconds().toString().padStart(2, '0');
-    timecodems.innerHTML        = timeActual.getUTCMilliseconds().toString().padStart(3, '0');
-    cliplength.innerHTML        = `${average(estSizeBuffer)/1000}s`;
+    timecodeHours.innerHTML     = timeActual.getUTCHours().toString().padStart(2, '0');
+    timecodeMinutes.innerHTML   = timeActual.getUTCMinutes().toString().padStart(2, '0');
+    timecodeSeconds.innerHTML   = timeActual.getUTCSeconds().toString().padStart(2, '0');
+    timecodeMS.innerHTML        = timeActual.getUTCMilliseconds().toString().padStart(3, '0');
+    clipLength.innerHTML        = `${average(estSizeBuffer)/1000}s`;
 
     if (timeActual.getTime() / 1000 <= 11) {
         table.style.color = "#ff4545";
-        tableborder.style.borderColor = "#ff4545";
+        tableBorder.style.borderColor = "#ff4545";
     } else {
         table.style.color = "#45ff45";
-        tableborder.style.borderColor = "#4b5457";
+        tableBorder.style.borderColor = "#4b5457";
     }
     // console.log(`pos: ${pos}\taverage: ${a}\ti: ${interval}\ttime: ${d}\ttimeAverage: ${ta}\ttimeActual: ${t}\ttimeTotal: ${average(totalArray)}\ttimeLeft: ${timeLeft}`);
 }
